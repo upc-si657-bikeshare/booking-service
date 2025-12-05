02ff1c3e47189d3d9088171aab74b07fc2c70f2b
ALAYA1803 <rodrigoalaya_2004@hotmail.com>
2025-12-05 15:11:31 -0500

    fix(application-properties): config swagger

--- src/main/java/com/bikeshare/booking/client/BikeClient.java
+++ src/main/java/com/bikeshare/booking/client/BikeClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.math.BigDecimal;
 
-@FeignClient(name = "catalog-service", url = "http://localhost:8082")
+@FeignClient(name = "catalog-service")
 public interface BikeClient {
 
     @GetMapping("/api/bikes/{id}")
